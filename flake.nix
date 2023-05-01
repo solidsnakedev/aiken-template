ef79a660163ec465a962db2926e6238b13d6ca4e
solidsnakedev <jona.ca.eng@gmail.com>
2023-05-01 09:47:53 -0400

    add nix develop env

--- flake.nix
+++ flake.nix
@@ -3,42 +3,28 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    # cargo2nix.url = "github:cargo2nix/cargo2nix/unstable";
-    # rust-overlay.url = "github:oxalica/rust-overlay";
     aiken = {
       url = "github:aiken-lang/aiken";
-      inputs = {
-        cargo2nix.url = "github:cargo2nix/cargo2nix/unstable";
-      };
     };
-      # inputs.nixpkgs.follows = "nixpkgs";
-    # };
-    # aiken.url = "github:solidsnakedev/aiken";
   };
 
   outputs = inputs@{ self, nixpkgs, flake-utils, aiken }:
     flake-utils.lib.eachDefaultSystem (system:
-    let pkgs = import nixpkgs { inherit system; overlays = [overlay]; };
-        # aikenPkgs = aiken.packages.${system};
-        # aiken-lib = pkgs.callPackage aiken {};
+    let 
         overlay = final : prev: {
-          aiken = inputs.aiken.packages.${system};
+          aiken = inputs.aiken.packages.${system}.default;
+          nodejs = prev.nodejs-18_x;
+          pnpm = prev.nodePackages.pnpm;
         };
-    in
+        pkgs = import nixpkgs { inherit system; overlays = [overlay ]; };
+    in rec
       {
-        # packages = rec {
-        #   hello = pkgs.hello;
-        #   default = aikenPkgs.aiken;
-        # };
-        packages = pkgs.aiken;
-        # packages = aiken-lib.buildPackage ./.;
-        # apps = rec {
-          # hello = flake-utils.lib.mkApp { drv = self.packages.${system}.hello; };
-          # default = hello;
-        # };
-        devShell = pkgs.aiken;
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [ hello aiken];
+          packages = [ pkgs.nodejs pkgs.pnpm pkgs.aiken] ;
+          shellHook = ''
+            echo "Aiken Dev Environment"
+            echo "Aiken Version: `${pkgs.aiken}/bin/aiken --version`"
+            '';
         };
       }
     );
